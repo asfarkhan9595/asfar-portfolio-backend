b5bc0e7b12974670578a9d99cebed16752c38d28
Asfar Khan <asfarkhan9595@gmail.com>
2026-09-12 11:28:54 +0530

    Restore Tailwind CDN script tag in admin layout for proper UI styling

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -11,6 +11,15 @@
             document.documentElement.classList.remove('dark');
         }
     </script>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script>
+        tailwind.config = {
+            darkMode: 'class',
+        }
+    </script>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    <script src="https://unpkg.com/lucide@latest"></script>
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
